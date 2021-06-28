816414fd6f06641067297592355c829f05818a0d
JorgeScp <jorge.molano28@gmail.com>
2021-06-28 17:33:20 -0500

    second

--- interview/models.py
+++ interview/models.py
@@ -26,19 +26,19 @@ class Interview(models.Model):
     comunicacion_4 = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
     comunicacion_5 = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
 
-    comunicacion_r = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
+    comunicacion_r = models.IntegerField(null=True, blank=True,validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
     #__________________2
     trabajo_equipo_1 = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
     trabajo_equipo_2 = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
     trabajo_equipo_3 = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
 
-    trabajo_equipo_r = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
+    trabajo_equipo_r = models.IntegerField(null=True, blank=True,validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
     #__________________3
     servicio_cliente_1 = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
     servicio_cliente_2 = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
     servicio_cliente_3 = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
 
-    servicio_cliente_r = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
+    servicio_cliente_r = models.IntegerField(null=True, blank=True,validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
     #__________________4
     resolucion_problemas_1 = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
     resolucion_problemas_2 = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
@@ -46,7 +46,7 @@ class Interview(models.Model):
     resolucion_problemas_4 = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
     resolucion_problemas_5 = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
 
-    resolucion_problemas_r = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
+    resolucion_problemas_r = models.IntegerField(null=True, blank=True,validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
     #__________________5
     liderazgo_1 = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
     liderazgo_2 = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
@@ -54,29 +54,29 @@ class Interview(models.Model):
     liderazgo_4 = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
     liderazgo_5 = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
 
-    liderazgo_r = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
+    liderazgo_r = models.IntegerField(null=True, blank=True,validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
     #__________________6
     admon_tiempo_1 = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
     admon_tiempo_2 = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
     admon_tiempo_3 = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
 
-    admon_tiempo_r = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
+    admon_tiempo_r = models.IntegerField(null=True, blank=True,validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
     #__________________7
     pensamiento_estrategico_1 = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)	 
     pensamiento_estrategico_2 = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)	 
     pensamiento_estrategico_3 = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)	 
     pensamiento_estrategico_4 = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)	 
 
-    pensamiento_estrategico_r = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)	 
+    pensamiento_estrategico_r = models.IntegerField(null=True, blank=True,validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)	 
     #__________________8
     enfoque_resultados_1 = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
     enfoque_resultados_2 = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
     enfoque_resultados_3 = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
     enfoque_resultados_4 = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
     
-    enfoque_resultados_r = models.IntegerField(validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
+    enfoque_resultados_r = models.IntegerField(null=True, blank=True,validators=[MinValueValidator(MIN), MaxValueValidator(MAX)],default=100)
     #=========================================================================================
-    result = models.CharField(max_length=50,default="0")
+    result = models.CharField(null=True, blank=True,max_length=50,default="0")
     #=========================================================================================
     strengths = models.CharField(null=True, blank=True,max_length=200,default="0")
     oportunities_areas = models.CharField(null=True, blank=True,max_length=200,default="0")
